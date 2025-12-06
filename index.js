c57fc21d9329eb64602c4c531ca77ea891f925a7
Thiago <thiagoalexsanderluiz@gmail.com>
2025-12-06 11:32:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/hello', (req, res) => res.send('Hello Azure World'));
+app.get('/', (req, res) => res.send('OK'));
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
