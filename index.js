16a7c5a4e62299aa5f2aa331d722919d7e3bfee3
Thiago <thiagoalexsanderluiz@gmail.com>
2025-12-07 00:33:11 -0300

    adding route that responds with 500 for testing

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.get('/hello', (req, res) => res.send('Hello Azure World'));
+
 app.get('/', (req, res) => res.send('OK'));
+
+app.get('/force500', (req, res) => {
+  res.status(500).send('Simulated server error');
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
